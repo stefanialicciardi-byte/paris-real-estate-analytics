1a29281be41c2bd74150dca35f9ac2530a8cec5b
tori <victoriafford@gmail.com>
2026-05-05 18:18:27 +0200

    Update and rename 7_Key_Takeaways__and_Learnings.py to 7_Conclusion.py

--- pages/7_Conclusion.py
+++ pages/7_Conclusion.py
@@ -19,7 +19,7 @@ pio.templates.default = "plotly_white"
 # Page config
 # -------------------------
 st.set_page_config(
-    page_title="Key Takeaways and Lessons Learned",
+    page_title="Conclusion",
     page_icon=":bulb:",
     layout="wide",
 )
@@ -27,7 +27,7 @@ st.set_page_config(
 # -------------------------
 # Header
 # -------------------------
-st.title("Key Takeaways and Learnings")
+st.title("Conclusion")
 st.caption(
     "In this final section, we summarize our key analytical findings and lessons learned from the project."
 )
@@ -37,7 +37,7 @@ st.caption(
 # -------------------------
 st.markdown("---") 
 
-st.header("Key Takeaways from the Data")
+st.header("Key findings from the data")
 st.markdown("...")
 
 st.markdown("---") 
@@ -46,7 +46,7 @@ st.markdown("---")
 #  Challenges and Lessons Learned
 # -------------------------
 
-st.header("Challenges and Lessons Learned")
+st.header("Challenges and solutions going forward")
 
 r1c1, r1c2 = st.columns(2)
 
@@ -56,9 +56,12 @@ with r1c1:
         <div style="border:1px solid #e5e7eb; border-radius:8px; padding:16px;">
         <strong>Manual Uploads Slow Pipeline and Increase Risk of Error</strong><br>
         <span style="color:#6b7280; font-size:0.9rem;">
-        * The main weakness of our ETL pipeline is the manual uploads involved at multiple steps in the pipeline. 
-        * To ensure consistency, there are checkpoints along the way were necessary data uploads are flagged.
-        * Looking forward, one solution would be to integrate the entire pipeline into Snowflake (which was not possible with a trial account).
+        
+        The main weakness of our ETL pipeline is the manual uploads involved at multiple steps in the pipeline. 
+        
+        To ensure consistency, there are checkpoints along the way were necessary data uploads are flagged.
+        
+        Looking forward, one solution would be to integrate the entire pipeline into Snowflake (which was not possible with a trial account).
         </span>
         </div>
         """, unsafe_allow_html=True,
@@ -69,9 +72,12 @@ with r1c2:
         <div style="border:1px solid #e5e7eb; border-radius:8px; padding:16px;">
         <strong>Joining Geographic Data</strong><br>
         <span style="color:#6b7280; font-size:0.9rem;">
-        * One significant challenge we faced is how to integrate geographic data into our schema. Since the relationships between variables are spacial, a traditional primary key / foreign key relationship isn't applicable.
-        * Since the scope of our project didn't require a full geospatial schema, our hybrid star model sufficed.
-        * Looking forward, if the further development of a geospatial layer is key aspect of the analysis, working with PostgreSQL and PostGIS would be a more suitable option.
+        
+        One significant challenge we faced is how to integrate geographic data into our schema. Since the relationships between variables are spacial, a traditional primary key / foreign key relationship isn't applicable.
+        
+        Since the scope of our project didn't require a full geospatial schema, our hybrid star model sufficed.
+        
+        Looking forward, if the further development of a geospatial layer is key aspect of the analysis, working with PostgreSQL and PostGIS would be a more suitable option.
         </span>
         </div>
         """, unsafe_allow_html=True,
